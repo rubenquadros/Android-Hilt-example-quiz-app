a3714248c554e5f3f578b14d3716a48906b3a07e
rubenquadros <rquadros95@gmail.com>
2021-06-19 11:48:44 +0530

    adding splash activity

--- app/src/main/java/com/ruben/funed/presentation/SplashActivity.kt
+++ app/src/main/java/com/ruben/funed/presentation/SplashActivity.kt
@@ -4,9 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ruben.funed.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
   }
 }
