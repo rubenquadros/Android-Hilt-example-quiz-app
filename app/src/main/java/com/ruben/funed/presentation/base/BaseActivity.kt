104cea4de534e54e50ca00b811a84d054c74e48a
rubenquadros <rquadros95@gmail.com>
2021-06-21 01:14:08 +0530

    adding a crash fallback

--- app/src/main/java/com/ruben/funed/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/ruben/funed/presentation/base/BaseActivity.kt
@@ -1,7 +1,9 @@
 package com.ruben.funed.presentation.base
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.ProgressBar
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.snackbar.Snackbar
+import com.ruben.funed.presentation.instruction.InstructionsActivity
 
 /**
  * Created by ruben.quadros on 19/06/21.
@@ -17,6 +20,7 @@ open class BaseActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.setDefaultUncaughtExceptionHandler { _, e -> handleUncaughtException(e) }
     }
 
     fun setupToolbar(toolbar: Toolbar, titleTv: AppCompatTextView, title: String, isBack: Boolean) {
@@ -47,5 +51,12 @@ open class BaseActivity: AppCompatActivity() {
             snackBar.dismiss()
         }
         snackBar.show()
+    }
+
+    private fun handleUncaughtException(e: Throwable) {
+        Log.e(this::javaClass.name, e.toString())
+        val intent = Intent(this, InstructionsActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
